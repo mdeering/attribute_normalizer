1b13fd2dce85b78a9b97807a3d71f2ec4dc73e17
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2010-08-19 20:11:14 +0800

    changed spec to use a better matcher for respond_to

--- spec/attribute_normalizer_spec.rb
+++ spec/attribute_normalizer_spec.rb
@@ -3,13 +3,12 @@ require File.dirname(__FILE__) + '/test_helper'
 describe AttributeNormalizer do
 
   it 'should add the class method Class#normalize_attributes and Class#normalize_attribute when included' do
-
     klass = Class.new do
       include AttributeNormalizer
     end
 
-    klass.respond_to?(:normalize_attributes).should be_true
-    klass.respond_to?(:normalize_attribute).should  be_true
+    klass.should respond_to(:normalize_attributes)
+    klass.should respond_to(:normalize_attribute)
   end
 
 end
