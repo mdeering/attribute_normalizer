ebfde2d974b76530c88113b141cb5afa91e99423
Dimko <deemox@gmail.com>
2013-03-19 14:48:07 +0400

    added spec for custom writers on active record columns

--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -8,4 +8,10 @@ describe Publisher do
     it { should normalize_attribute(:phone_number).from('+ 1 (877) 987-9875').to('18779879875') }
   end
 
+  context 'on custom writer method' do
+    subject { Publisher.new(:name => 'Mike') }
+    it { should normalize_attribute(:name).from('').to(nil) }
+    its(:custom_writer) { should be_true }
+  end
+
 end
