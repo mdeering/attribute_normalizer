e1638aa5967fce7afd5fb315a82bb381386ce265
Nurahmadie <nurahmadie@gmail.com>
2013-08-31 07:06:31 +0700

    Remove whitespaces adjacent to newline. Actually pass all tests

--- lib/attribute_normalizer/normalizers/whitespace_normalizer.rb
+++ lib/attribute_normalizer/normalizers/whitespace_normalizer.rb
@@ -2,7 +2,7 @@ module AttributeNormalizer
   module Normalizers
     module WhitespaceNormalizer
       def self.normalize(value, options = {})
-        value.is_a?(String) ? value.gsub(/[^\S\n]+/, ' ').gsub(/\s+\n$/, "\n").strip : value
+        value.is_a?(String) ? value.gsub(/[^\S\n]+/, ' ').gsub(/\s?\n\s?/, "\n").strip : value
       end
     end
   end
