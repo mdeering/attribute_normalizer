58ec2d422aa734346f5004cc3f81b7aa8c9b52e7
Samuel Lebeau <samuel.lebeau@gmail.com>
2014-03-09 19:04:58 +0100

    Use squeeze instead of gsub in SquishNormalizer

--- lib/attribute_normalizer/normalizers/squish_normalizer.rb
+++ lib/attribute_normalizer/normalizers/squish_normalizer.rb
@@ -2,7 +2,7 @@ module AttributeNormalizer
   module Normalizers
     module SquishNormalizer
       def self.normalize(value, options = {})
-        value.is_a?(String) ? value.strip.gsub(/\s+/, ' ') : value
+        value.is_a?(String) ? value.strip.squeeze : value
       end
     end
   end
