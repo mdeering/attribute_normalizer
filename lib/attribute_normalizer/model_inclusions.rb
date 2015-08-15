3a13cb4909eb6d34f3f235972244efc99ef35955
Michael Deering <mdeering@mdeering.com>
2015-08-15 12:57:33 -0600

    Remove remaining code for disabled default_attributes featuer

--- lib/attribute_normalizer/model_inclusions.rb
+++ lib/attribute_normalizer/model_inclusions.rb
@@ -66,19 +66,6 @@ module AttributeNormalizer
 
     alias :normalize_attribute :normalize_attributes
 
-    def normalize_default_attributes
-      AttributeNormalizer.configuration.default_attributes.each do |attribute_name, options|
-        normalize_attribute(attribute_name, options) if self.column_names.include?(attribute_name)
-      end
-    end
-
-    # def inherited(subclass)
-    #   super
-    #   if subclass.name.present? && subclass.respond_to?(:table_exists?) && (subclass.table_exists? rescue false)
-    #     subclass.normalize_default_attributes
-    #   end
-    # end
-
   end
 
 end
